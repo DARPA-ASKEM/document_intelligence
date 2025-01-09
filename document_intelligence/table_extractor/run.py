766a62c4c5d7c08cc493c07a94412cf6c921f0ee
David Gauldie <dgauldie@uncharted.software>
2025-01-09 09:44:31 -0500

    fix table extraction prompt to format extracted tables into XML

--- document_intelligence/table_extractor/run.py
+++ document_intelligence/table_extractor/run.py
@@ -13,9 +13,9 @@ from ultralyticsplus import YOLO
 app = FastAPI()
 model = YOLO('foduucom/table-detection-and-extraction')
 
-TABLE_EXTRACTION_PROMPT = """Please extract the tables from the images and provide the data in a tabular format. Some images may not contain tables, and may contain a mix of figures, graphs and equations. Please ignore these images and give them a score of 0. You will structure your response as a JSON object with the following schema:
+TABLE_EXTRACTION_PROMPT = """Please extract the tables from the images and provide the data in XML format. Some images may not contain tables, and may contain a mix of figures, graphs and equations. Please ignore these images and give them a score of 0. You will structure your response as a JSON object with the following schema:
 
-'table_text': The text extracted from the table, use visual cues to separate the columns and rows. Ensure that greek characters are preserved, do not swap "α" to "a" for example.
+'table_text': The XML formatted text extracted from the table, use visual cues to separate the columns and rows. Some cells may span multiple columns. Ensure that symbols and greek characters are preserved, do not swap "α" to "a" for example.
 'score': A score from 0 to 10 indicating the quality of the extracted table. 0 indicates that the image does not contain a table, 10 indicates a high-quality extraction.
 
 Begin:
@@ -34,7 +34,7 @@ def process_and_send_images(output_dict, prompt):
             img_type = "image/png"
 
             response = client.chat.completions.create(
-                model="gpt-4o-mini",
+                model="gpt-4o-2024-08-06",
                 messages=[
                     {
                         "role": "user",
